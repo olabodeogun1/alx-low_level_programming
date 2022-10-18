6a9be085082a841781d01e095ee463adc4135e9f
Olabode Ogun <olabodeogun1@gmail.com>
2022-10-18 15:16:52 -0700

    :

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabets
+ * @c: cn: 1 if character is a letter, 0 otherwise
+ */
+int _isalpha(int= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
