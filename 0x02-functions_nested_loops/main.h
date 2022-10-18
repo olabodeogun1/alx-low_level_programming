bb557d10086e596f62ced0cbf87086b6b36fe606
Olabode Ogun <olabodeogun1@gmail.com>
2022-10-18 14:20:35 -0700

    0x02-functions_nested

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,9 +8,10 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void Olabode_Ogun(void);
+void olabode_ogun(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+int _putchar(char c);
 
 #endif
