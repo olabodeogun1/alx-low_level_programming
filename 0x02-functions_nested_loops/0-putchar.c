cd489c532c6dabf77ed6220e2114ef90b22adf61
Olabode Ogun <olabodeogun1@gmail.com>
2022-10-18 09:21:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - print putchar
- * Return: always 0
+ * main - print _putchar and a new line
+ * Return: Always 0
  */
 int main(void)
 {
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-return (0);
+
+	return (0);
 }
