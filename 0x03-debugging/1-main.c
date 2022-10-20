a4d52ca4e651b55e397eb33b7cd05d10f9a983f3
Olabode Ogun <olabodeogun1@gmail.com>
2022-10-20 08:30:27 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h
+
+/**
+ * main - causes an infinite loop
+ * Retrn 0
+ */
+
+int main(void)
+{
+	int i;
+
+	print("Infinite loop incomint :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10);
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided: \\o/\");
+
+	return (0);
+}
