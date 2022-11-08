a6f18b5b709f860fba16705f223880140fd40ff2
Olabode Ogun <olabodeogun1@gmail.com>
2022-11-08 12:04:51 -0800

    clearclear

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,20 +8,13 @@
  */
 
 void free_grid(int **grid, int height)
-
 {
+	int i;
 
-		int i
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
 
-		for (i = 0; i < height; i++)
-		{
-
-								free(grid[i]);
-
-									}
-
-
-
-				free(grid);
-
+	free(grid);
 }
