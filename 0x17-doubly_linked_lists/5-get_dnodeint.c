10321b5db5d9f1b0b00291d1509070d2054d2da6
Olabode Ogun <olabodeogun1@gmail.com>
2023-03-20 05:36:51 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,8 +10,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
     unsigned int i;
     dlistint_t *current = head;
 
-    for (i = 0; i < index && current != NULL; i++)
+    for (i = 0; current != NULL && i < index; i++)
         current = current->next;
 
-    return (i == index ? current : NULL);
+    return (i == index && current != NULL ? current : NULL);
 }
+
